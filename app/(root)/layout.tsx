003f4df063ccd36a14019391e0309b178dcf0e7d
Aayush <aayushg2908@gmail.com>
2024-03-30 11:57:27 +0530

    Zoom clone using getstream sdk

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
